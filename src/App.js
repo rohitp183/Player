1f23da423a5c1a0e54e5cad18e0aee0a252092c4
Nishant kumar <nishants440@gmail.com>
2022-03-27 00:02:37 +0530

    Simple Music Player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { audios } from './audioData';
+import Player from './components/Player';
 function App() {
+  const songs = audios;
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
+  const nextSong = () => {
+    if(currentIndex + 1 < audios.length){
+      setCurrentIndex(currentIndex + 1)
+      setCurrentSong(audios[currentIndex + 1])
+    }
+  }
+
+  const prevSong = () => {
+    if(currentIndex > 0){
+      setCurrentIndex(currentIndex - 1)
+      setCurrentSong(audios[currentIndex - 1])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='player-main'>
+      <Player
+        currentSong={currentSong}
+        currentIndex={currentIndex}
+        nextSong={nextSong}
+        prevSong={prevSong}
+      />
     </div>
   );
 }
---
+++ src/audioData.js
@@ -0,0 +1,47 @@
+export const audios = [
+    {
+        name: 'Hoshiyar Rehna',
+        music: require('./audios/Hoshiyar-Rehna.mp3'),
+        creator: 'Kabir Cafe'
+    },
+    {
+        name: 'Manja',
+        music: require('./audios/Manja.mp3'),
+        creator: 'Amit Trivedi'
+    },
+    {
+        name: 'Mann Kasturi',
+        music: require('./audios/Mann-Kasturi.mp3'),
+        creator: 'Kabir Cafe'
+    },
+    {
+        name: 'Matkar Maya Ko Ahankar',
+        music: require('./audios/Matkar-Maya-Ko-Ahankar.mp3'),
+        creator: 'Kabir Cafe'
+    },
+    {
+        name: 'Kya Leke Aaya Jagat Mai',
+        music: require('./audios/Kya-Leke-Aaya-Jagat-Mai.mp3'),
+        creator: 'Kabir Cafe'
+    },
+    {
+        name: 'Agar Tum Saath Ho',
+        music: require('./audios/Agar-Tum-Saath-Ho.mp3'),
+        creator: 'A.R. RAHMAN, ALKA YAGNIK, ARIJIT SINGH'
+    },
+    {
+        name: 'Shayad ',
+        music: require('./audios/Shayad.mp3'),
+        creator: 'Pritam, Arijit Singh'
+    },
+    {
+        name: 'Ilahi',
+        music: require('./audios/Ilahi.mp3'),
+        creator: 'Arijit Singh, Pritam'
+    },
+    {
+        name: 'MONTA RE',
+        music: require('./audios/MONTA-RE.mp3'),
+        creator: 'SWANAND KIRKIRE, AMITABH BHATTACHARYA, AMIT TRIVEDI'
+    },
+].sort(() => .5 - Math.random())
